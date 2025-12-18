53caf3f66f65b207808a10b9b0bd608f44a276a0
panyushan <1193345664@qq.com>
2025-12-18 18:52:01 +0800

    perf

--- src/features/fund/account-settlement/hooks/use-account-amount.ts
+++ src/features/fund/account-settlement/hooks/use-account-amount.ts
@@ -1,16 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
 import { getAccountAmount } from '@/api/fund'
 import { useMerchantStore } from '@/stores/merchant-store'
+import { useCountryStore } from '@/stores'
 
 export const useAccountAmount = () => {
-  const { selectedMerchant } = useMerchantStore()
+  const { selectedCountry } = useCountryStore()
+      const { selectedMerchant } = useMerchantStore()
 
   return useQuery({
-    queryKey: ['accountAmount', selectedMerchant?.appid],
+    queryKey: ['accountAmount', selectedCountry?.code, selectedMerchant?.appid],
     queryFn: async () => {
       const response = await getAccountAmount()
       return response.result || {}
     },
-    enabled: !!selectedMerchant,
+    // enabled: !!selectedMerchant,
   })
 }
--- src/features/fund/account-settlement/hooks/use-exchange-rate.ts
+++ src/features/fund/account-settlement/hooks/use-exchange-rate.ts
@@ -1,9 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 import { getExchangeRate } from '@/api/fund'
+import { useCountryStore, useMerchantStore } from '@/stores'
 
 export const useExchangeRate = () => {
+  const { selectedCountry } = useCountryStore()
+    const { selectedMerchant } = useMerchantStore()
   return useQuery({
-    queryKey: ['exchangeRate'],
+    queryKey: ['exchangeRate',selectedCountry?.code, selectedMerchant?.appid],
     queryFn: async () => {
       const response = await getExchangeRate()
       return response.result || {}
